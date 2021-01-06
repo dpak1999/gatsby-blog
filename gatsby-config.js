75834a5344d16d16a0fd169440693d3bec80ada6
dpak1999 <deepakkumardash313@gmail.com>
2021-01-06 23:38:32 +0530

    add : site meta data for seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 /** @format */
 
 module.exports = {
-  plugins: ["gatsby-plugin-emotion"],
+  siteMetadata: {
+    title: "My blogs",
+    description:
+      "A site built using gatsby and react along with graphql and styled with emotion",
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
 };
---
+++ src/hooks/useSiteMetadata.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import { graphql, useStaticQuery } from "gatsby";
+
+const useSiteMetadata = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+
+  return data.site.siteMetadata;
+};
+
+export default useSiteMetadata;
