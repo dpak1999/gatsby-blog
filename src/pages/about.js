db08f511fbee639b8fde2288ba851dfd9b102fcd
dpak1999 <deepakkumardash313@gmail.com>
2021-01-06 20:56:34 +0530

    add : index & about pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <>
+    <h1>About me</h1>
+    <p>
+      This is where I occasionally scramble things<span role="img">😛😛😛</span>
+      ...
+    </p>
+    <Link to="/">&larr; back to home</Link>
+  </>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react";
+/** @format */
 
-export default () => <div>Hello there</div>;
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <>
+    <h1>Home</h1>
+    <p>
+      Hello there<span role="img">👋👋</span>
+    </p>
+    <Link to="/about">Learn more about me &rarr;</Link>
+  </>
+);
