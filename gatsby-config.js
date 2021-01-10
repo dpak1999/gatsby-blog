31892643ef1321bf70306585206691b16504b8e0
dpak1999 <deepakkumardash313@gmail.com>
2021-01-10 19:37:59 +0530

    add : mdx react plugin $ configuring it

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
     description:
       "A site built using gatsby and react along with graphql and styled with emotion",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+  ],
 };
