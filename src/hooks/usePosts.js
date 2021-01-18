5f16b702936791de99f9280fe7ce4f862d7cbe00
dpak1999 <deepakkumardash313@gmail.com>
2021-01-18 22:10:52 +0530

    getting posts on homepage

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import { graphql, useStaticQuery } from "gatsby";
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMdx {
+        nodes {
+          excerpt
+          frontmatter {
+            title
+            slug
+            author
+          }
+        }
+      }
+    }
+  `);
+
+  return data.allMdx.nodes.map((post) => ({
+    title: post.frontmatter.title,
+    author: post.frontmatter.author,
+    slug: post.frontmatter.slug,
+    excerpt: post.excerpt,
+  }));
+};
+
+export default usePosts;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,25 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import usePosts from "../hooks/usePosts";
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>
-      Hello there
-      <span role="img" aria-label="hand">
-        👋👋
-      </span>
-    </p>
-    <Link to="/about/">Learn more about me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>
+        Hello there
+        <span role="img" aria-label="hand">
+          👋👋
+        </span>
+      </p>
+      <Link to="/about/">Learn more about me &rarr;</Link>
+
+      <h2>Read my blogs</h2>
+      {posts.map((post) => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
+    </Layout>
+  );
+};
