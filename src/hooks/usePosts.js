2c315faf0bce3ce1105a32ce7de1d4668479bc20
dpak1999 <deepakkumardash313@gmail.com>
2021-01-19 22:43:54 +0530

    add : thumbnail images

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -12,6 +12,17 @@ const usePosts = () => {
             title
             slug
             author
+            image {
+              sharp: childImageSharp {
+                fluid(
+                  maxWidth: 100
+                  maxHeight: 100
+                  duotone: { shadow: "#663399", highlight: "#ddbbff" }
+                ) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
           }
         }
       }
@@ -22,6 +33,7 @@ const usePosts = () => {
     title: post.frontmatter.title,
     author: post.frontmatter.author,
     slug: post.frontmatter.slug,
+    image: post.frontmatter.image,
     excerpt: post.excerpt,
   }));
 };
